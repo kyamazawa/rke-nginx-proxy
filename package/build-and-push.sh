39256cc8bcbc708b2ff2368b2dde502a2762e580
moelsayed <m.elsayed@gmail.com>
2018-01-20 00:49:43 +0200

    listen on all interfaces

--- package/build-and-push.sh
+++ package/build-and-push.sh
@@ -2,5 +2,5 @@
 
 ACCT=${ACCT:-rancher}
 
-docker build -t $ACCT/rke-nginx-proxy:0.1.0 .
-docker push $ACCT/rke-nginx-proxy:0.1.0
+docker build -t $ACCT/rke-nginx-proxy:v0.1.1 .
+docker push $ACCT/rke-nginx-proxy:v0.1.1
